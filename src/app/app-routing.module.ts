34efd2d3ec2adc88885c81170a2ae5bc5579b3d1
Anthony Pham <anthonytpham31@gmail.com>
2022-07-11 07:12:18 -0700

    add routing to guildgifts from home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GuildGiftDataComponent } from './guild-gift-data/guild-gift-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'GuildGifts', component: GuildGiftDataComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
